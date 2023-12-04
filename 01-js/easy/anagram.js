31dbbb5ddd3778608a6ccc58f7f5f7eb91f94f1c
Aditya Dey <deyaditya34@gmail.com>
2023-12-04 22:24:40 +0530

    easy part completed

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,6 +5,37 @@
 */
 
 function isAnagram(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  let str1LowerCase = str1.toLowerCase();
+  let str2LowerCase = str2.toLowerCase();
+
+  let found = false;
+  for (let i = 0; i < str1LowerCase.length; i++) {
+    for (let j = 0; j < str2LowerCase.length; j++) {
+      if (str1LowerCase[i] === str2LowerCase[j]) {
+        found = true;
+      }
+    }
+    if (found === false) {
+      return false;
+    }
+  }
+
+  for (let i = 0; i < str2LowerCase.length; i++) {
+    for (let j = 0; j < str1LowerCase.length; j++) {
+      if (str2LowerCase[i] === str1LowerCase[j]) {
+        found = true;
+      }
+    }
+    if (found === false) {
+      return found;
+    }
+  }
+  
+  return found;
 
 }
 
--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -5,8 +5,60 @@
   Output - [{ category1 - total_amount_spent_on_category1 }, { category2 - total_amount_spent_on_category2 }]
 */
 
-function calculateTotalSpentByCategory(transactions) {
-  return [];
+function calculateTotalSpentByCategory(transactions = []) {
+  let result = [];
+
+  let found = false;
+
+  for (let i = 0; i < transactions.length; i++) {
+    for (let j = 0; j < result.length; j++) {
+      if (result.length > 0) {
+        if (transactions[i]["category"] === result[j]["category"]) {
+          found = true;
+        }
+      }
+    }
+    if (found === false) {
+      result.push({ category: transactions[i]["category"], "totalSpent": 0 });
+    }
+    found = false;
+  }
+
+  for (let i = 0; i < transactions.length; i++) {
+    for (let j = 0; j < result.length; j++) {
+      if (transactions[i]["category"] === result[j]["category"]) {
+        Reflect.set(result[j], "totalSpent", result[j]["totalSpent"] + transactions[i]["price"])
+      }
+    }
+  }
+
+  return result;
 }
+
+console.log(
+  calculateTotalSpentByCategory([
+    {
+      id: 1,
+      timestamp: 1656076800000,
+      price: 10,
+      category: 'Food',
+      itemName: 'Pizza',
+    },
+    {
+      id: 2,
+      timestamp: 1656105600000,
+      price: 20,
+      category: 'Food',
+      itemName: 'Burger',
+    },
+    {
+      id: 3,
+      timestamp: 1656134400000,
+      price: 30,
+      category: 'Food',
+      itemName: 'Sushi',
+    },
+  ])
+);
 
 module.exports = calculateTotalSpentByCategory;
